cc477788227d5a02dc7d8181ef315f2228bb4c96
Raman Navarych <r.navarych@softteco.com>
2020-01-15 11:18:05 +0300

    DefaultCallout Component

--- src/components/DefaultMarker/index.js
+++ src/components/DefaultMarker/index.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import { Marker, Callout, CalloutSubview } from 'react-native-maps'
+import { Marker } from 'react-native-maps'
 
-import styles from './styles'
-import * as routes from '../../constants/routes'
+import DefaultCallout from '../DefaultCallout';
 
 const DefaultMarker = (props) => {
   const { index, station, navigation } = props
-
-  const getStationIDBasedOnENV = (station) => {
-    //TODO: Should be deleted when the production API will be ready
-    return __DEV__ ? station.asset_id : station.id
-  }
-
   return (
     <Marker
       key={index}
@@ -23,19 +15,7 @@ const DefaultMarker = (props) => {
       title={station.the_geom.coordinates.title}
       description={station.the_geom.map_label}
     >
-      <Callout tooltip>
-        <View style={styles.calloutContainer}>
-          <Text style={styles.bicyclesAmountText}>Total: {station.total_amount_of_bicycles}</Text>
-          <Text style={styles.bicyclesAmountText}>Available: {station.amount_of_bicycle_available}</Text>
-          <CalloutSubview
-            style={styles.calloutButton} 
-            onPress={() => navigation.navigate(routes.STATION_INFO_SCREEN, { 
-              stationID: getStationIDBasedOnENV(station)
-            })}>
-            <Text style={styles.buttonText}>See Details</Text>
-          </CalloutSubview>
-        </View>
-      </Callout>
+      <DefaultCallout station={station} navigation={navigation}/>
     </Marker>
   )
 }
